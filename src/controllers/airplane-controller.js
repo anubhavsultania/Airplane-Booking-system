99eb040a55f54997e9101fd5ead4c7e789557f4a
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-08-20 19:46:25 +0530

    Added get airplane api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -28,4 +28,15 @@ async function getAirplanes(req, res) {
   }
 }
 
-module.exports = { createAirplane, getAirplanes };
+async function getAirplane(req, res) {
+  try {
+    const airplanes = await AirplaneService.getAirplane(req.params.id);
+    SuccessResponse.data = airplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+module.exports = { createAirplane, getAirplanes, getAirplane };
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -25,8 +25,8 @@ async function createAirplane(data) {
 
 async function getAirplanes() {
   try {
-    const airplane = await airplaneRepository.getAll();
-    return airplane;
+    const airplanes = await airplaneRepository.getAll();
+    return airplanes;
   } catch (error) {
     throw new AppError(
       "Cannot fetch data of all the airplane",
@@ -35,4 +35,22 @@ async function getAirplanes() {
   }
 }
 
-module.exports = { createAirplane, getAirplanes };
+async function getAirplane(id) {
+  try {
+    const airplane = await airplaneRepository.get(id);
+    return airplane;
+  } catch (error) {
+    if (error.statusCode === StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airplane you requested is not present",
+        error.statusCode,
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of the airplane",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+module.exports = { createAirplane, getAirplanes, getAirplane };

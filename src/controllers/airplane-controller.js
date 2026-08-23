9e2b5b109e01ec5a2c96157aa5e82e331a195002
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-08-23 18:06:07 +0530

    Added create and delete city api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,6 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
 const { AirplaneService } = require("../services");
-const { response } = require("express");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
 
 async function createAirplane(req, res) {
---
+++ src/services/city-service.js
@@ -0,0 +1,50 @@
+const { CityRepository } = require("../repositories");
+const AppError = require("../utils/errors/app-error");
+const StatusCodes = require("http-status-codes");
+
+const cityRepository = new CityRepository();
+
+async function createCity(data) {
+  try {
+    const city = await cityRepository.create(data);
+    return city;
+  } catch (error) {
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
+      let explanation = [];
+      error.errors.forEach((err) => {
+        explanation.push(err.message);
+      });
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+    }
+    throw new AppError(
+      "Cannot create a new city object",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+async function destroyCity(id) {
+  try {
+    const response = await cityRepository.destroy(id);
+    return response;
+  } catch (error) {
+    if (error.statusCode === StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The city you requested to delete is not present",
+        error.statusCode,
+      );
+    }
+    throw new AppError(
+      "Cannot delete the city",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+module.exports = {
+  createCity,
+  destroyCity,
+};

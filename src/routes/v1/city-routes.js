b05fe0fe8cb918a51739e1a427b5561d6b59a01f
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-08-23 18:22:20 +0530

    Added middleware for createCity

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { CityController } = require("../../controllers");
+const { CityMiddleware } = require("../../middlewares");
 const router = express.Router();
 
-router.post("/", CityController.createCity);
+router.post(
+  "/",
+  CityMiddleware.validateCreateRequest,
+  CityController.createCity,
+);
 router.delete("/:id", CityController.destroyCity);
 module.exports = router;
